d610fac3703b5695ffd897a69b01c3ecdc1eae3e
vasergen <vasergen@gmail.com>
2016-02-27 00:09:54 +0100

    add es6 constructor

---
+++ patterns/constructor.es6.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/**
+* Pattern: Constructor (Es6 syntax)
+*/
+
+class Car {
+  constructor(model, year) {
+    this.model = model
+    this.year = year
+  }
+  toString() {
+    return `Model: ${this.model}, Year: ${this.year}`
+  }
+}
+
+let car = new Car('Honda', 1989)
+
+console.log(car.toString());
