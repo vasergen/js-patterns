8e5384f193ac7ab3f374642236e8152fde1bd5d3
vasergen <vasergen@gmail.com>
2016-02-27 00:41:19 +0100

    added module pattern

---
+++ patterns/module.js
@@ -0,0 +1,38 @@
+'use strict'
+/**
+* Pattern: Module
+*/
+
+let counter = (function() {
+  //Private properties
+  let count = 0
+
+  //Private methods
+  function _inc() {
+    return ++count
+  }
+
+  function _dec() {
+    return --count
+  }
+
+  function _reset() {
+    count = 0
+  }
+
+  function _print() {
+    console.log(`Count = ${count}`);
+  }
+
+  //Public API
+  return {
+    inc: _inc,
+    dec: _dec,
+    reset: _reset,
+    print: _print
+  }
+})()
+
+counter.inc()
+counter.inc()
+counter.print()
