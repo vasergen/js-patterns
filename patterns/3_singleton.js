0fa56a2e0a9e1020049ad594cb1061bc849aac40
vasergen <vasergen@gmail.com>
2016-02-28 02:35:37 +0100

    minor changes

--- patterns/3_singleton.js
+++ patterns/3_singleton.js
@@ -4,43 +4,37 @@
 * Pattern: Singleton
 */
 
-//Some Class which later will be our singleton instance
-function Counter() {
-  let _initValue = 0
-  let _count = _initValue
-
-  this.inc = function () {
-    return ++_count
-  }
-  this.dec = function() {
-    return --_count
-  }
-  this.reset = function() {
-    count = _initValue
-  }
-  this.print = function() {
-    console.log(`Count: ${_count}`)
-  }
-}
-
-//Singleton Wrapper
-function SingletonWrapper(ClassName) {
+//Singleton
+function Singleton(ClassName) {
   let instance
 
   return {
     getInstance() {
-      if(!instance) {
+      if(!instance)
         instance = new ClassName()
-      }
 
       return instance
     }
   }
 }
 
-//Singleton
-let CounterSingleton = SingletonWrapper(Counter)
+//Counter - some class which later will be our singleton instance
+function Counter() {
+  let _initValue = 0
+  let _count = _initValue
 
+  return {
+    inc() { return ++_count },
+    dec() { return --_count },
+    reset() { count = _initValue },
+    print() { console.log(`Count: ${_count}`) }
+  }
+}
+
+//CounterSingleton
+let CounterSingleton = Singleton(Counter)
+
+//Test
 let countA = CounterSingleton.getInstance()
 let countB = CounterSingleton.getInstance()
 countA.inc()
